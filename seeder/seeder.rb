22d3591acc192e6678f1eb16dea7716252a3e44f
ER <colorfulsing@gmail.com>
2020-04-27 19:22:07 -0600

    Add input and env vars tests.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -28,3 +28,24 @@ outputs << {
   bar: "bar value 1",
   from: "seeder"
 }
+
+outputs << {
+  _collection: "from_seeder",
+  _id: "input_vars",
+  start_var1: ENV["start_var1"],
+  start_with_env_var2: ENV["start_with_env_var2"],
+  start_with_global_env_var3: ENV["start_with_global_env_var3"],
+  start_with_env_var4: ENV["start_with_env_var4"],
+  start_with_default_var5: ENV["start_with_default_var5"],
+  start_optional_var6: ENV["start_optional_var6"]
+}
+
+outputs << {
+  _collection: "from_seeder",
+  _id: "env_vars".
+  start_with_env_var2: ENV["start_with_env_var2"],
+  start_with_global_env_var3: ENV["start_with_global_env_var3"],
+  start_with_env_var4: ENV["start_with_env_var4"],
+  my_env1: ENV["my_env1"],
+  my_required_env2: ENV["my_required_env2"]
+}
